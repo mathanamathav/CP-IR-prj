9b4fbd12a0515c1749488810740e4289d9fcecc0
Mathana Mathav <asmathav01@gmail.com>
2023-11-01 11:44:37 +0530

    page rank implementation

---
+++ pages/2_👊_Ranking_CP_Question.py
@@ -0,0 +1,104 @@
+import streamlit as st
+from utils import random_walk , preprocess_convert_graph , plot_rank_graph , adj_rank , eign_rank
+import pandas as pd
+import networkx as nx
+
+st.set_page_config(page_title="Ranking", page_icon="1️⃣", layout="wide")
+
+st.markdown(
+    """
+    <style>
+    .stApp {
+        max-width: 100%;
+        padding: 1rem;
+    }
+    .stButton>button {
+        background-color: #008B8B;
+        color: white;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    """
+    # Ranking Pages
+
+    In an Information Retrieval (IR) project managing a coding question dataset, the aim is to suggest relevant and high-quality coding problems to users. 
+    Relevance is determined by alignment with user-selected topics, while quality is assessed based on user success rates. 
+    This approach is reminiscent of PageRank, Google's original search engine algorithm, which improved web search results by considering content relevance 
+    and link quality. The IR project leverages data-driven insights to enhance the user's learning experience by recommending problems aligned with their 
+    interests and ranking them based on their success rates.
+    """
+)
+
+G = preprocess_convert_graph()
+pagerank = nx.pagerank(G)
+st.plotly_chart(plot_rank_graph(G , "Networkx Package" ,pagerank) , use_container_width=True)
+
+st.markdown(
+    """
+    ## Random Walk
+
+    In the context of our project, we employ a random walk with a parameter called alpha to ensure efficient navigation through the coding question dataset. 
+    This random walk involves selecting a node and choosing one of its out-links, while also allowing for occasional random jumps to other nodes. 
+    The alpha parameter controls the likelihood of following links, with (1-alpha) governing the probability of making random jumps. 
+    This approach helps us avoid getting stuck in loops and efficiently explores the dataset, ultimately providing us with a counter that normalizes node 
+    visit frequencies, which can be used to approximate PageRank-like results for ranking coding problems.
+    """
+)
+
+ranks_rw = random_walk(G)
+
+st.plotly_chart(plot_rank_graph(G , "Random Walk" ,ranks_rw) , use_container_width=True)
+
+s1 = pd.Series(pagerank.values())
+s2 = pd.Series(ranks_rw)
+
+df = pd.DataFrame(dict(PageRank=s1, RandomWalk=s2))
+df['Diff'] = df['RandomWalk'] - df['PageRank']
+df = df*100
+st.dataframe(df,use_container_width=True)
+
+st.markdown(
+    """
+    ## Adjacency Matrix
+
+    The random walk implementation of PageRank is conceptually simple, 
+    but not very efficient to compute. An alternative is to use a matrix to represent the links 
+    from each node to every other node, and compute the eigenvectors of that matrix.
+    """
+)
+
+ranks_am = adj_rank(G)
+
+s1 = pd.Series(pagerank.values())
+s2 = pd.Series(ranks_am)
+
+df = pd.DataFrame(dict(PageRank=s1, AdjMatrix=s2))
+df['Diff'] = df['AdjMatrix'] - df['PageRank']
+df = df*100
+st.dataframe(df,use_container_width=True)
+
+
+st.markdown(
+    """
+    ## Eigenvectors
+
+    If you start with almost any vector and multiply by a matrix repeatedly,as we did in the previous section, the result will converge to the eigenvector of the matrix that corresponds to the largest eigenvalue.
+    In fact, repeated multiplication is one of the algorithms used to compute eigenvalues: it is called power iteration.
+    Instead of using an iterative method, we can also compute eigenvalues directly, 
+    which is what the Numpy function eig does. Here are the eigenvalues and eigenvectors of the Google matrix.
+    """
+)
+
+ranks_ev = eign_rank(G)
+
+s1 = pd.Series(pagerank.values())
+s2 = pd.Series(ranks_ev)
+
+df = pd.DataFrame(dict(PageRank=s1, Eigenvector=s2))
+df['Diff'] = df['Eigenvector'] - df['PageRank']
+df = df*100
+st.dataframe(df,use_container_width=True)
--- utils.py
+++ utils.py
@@ -1,15 +1,134 @@
 import pandas as pd
+import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
+from collections import Counter
+import plotly.graph_objs as go
+from plotly.subplots import make_subplots
+import networkx as nx
 
+def eign_rank(G):
+    M = nx.to_numpy_array(G)
+    alpha = 0.85
 
-# Function to recommend similar questions
+    N = len(G)
+    P = np.ones((N, N)) / N
+    M = alpha * M + (1 - alpha) * P
+
+    eigenvalues, eigenvectors = np.linalg.eig(M.T)
+    ind = np.argmax(eigenvalues)
+    largest_eigenvector = np.real(eigenvectors[:, ind])
+    ranks_ev = largest_eigenvector / largest_eigenvector.sum()
+    
+    return ranks_ev
+
+def adj_rank(G):
+    M = nx.to_numpy_array(G)
+    N = len(G)
+
+    p = np.full(N, 1/N)
+    alpha = 0.85
+    GM = alpha * M + (1 - alpha) * p
+    x = np.full(N, 1/N)
+
+    for i in range(10):
+        x = GM.T @ x
+
+    ranks_am = x / x.sum()
+    return ranks_am
+
+def preprocess_convert_graph():
+    df = pd.read_csv("Data/leetcode_questions.csv")
+    graph_df = df[['Question ID' , 'Similar Questions ID' , 'Question Title']]
+    graph_df.dropna( inplace = True )
+
+    graph_df['Similar Questions ID'] = graph_df['Similar Questions ID'].str.split(',')
+    graph_df = graph_df.explode('Similar Questions ID')
+    graph_df = graph_df.rename(columns={'Similar Questions ID': 'Similar Question ID'})
+    graph_df = graph_df.reset_index(drop=True)
+    graph_df['Similar Question ID'] = graph_df['Similar Question ID'].astype('int')
+    G = nx.from_pandas_edgelist(graph_df, "Question ID", "Similar Question ID")
+    return G
+
+def plot_rank_graph(G , title , ranks):
+    fig = make_subplots(rows=1, cols=1)
+    pos = nx.spring_layout(G)
+
+    # Create node positions for Plotly
+    node_x = [pos[k][0] for k in G.nodes]
+    node_y = [pos[k][1] for k in G.nodes]
+
+    # Create a list of labels with PageRank values
+    labels = [f'Node {k}<br>PageRank: {ranks[k]:.3f}' for k in G.nodes]
+
+    # Create a scatter plot for nodes
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        marker=dict(size=[v * 5000 for v in ranks.values()], showscale=True, colorscale='YlGnBu' ),
+        text=labels,
+        hoverinfo='text'
+    )
+
+    # Create edges
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():
+        x0, y0 = pos[edge[0]]
+        x1, y1 = pos[edge[1]]
+        edge_x.append(x0)
+        edge_x.append(x1)
+        edge_x.append(None)
+        edge_y.append(y0)
+        edge_y.append(y1)
+        edge_y.append(None)
+
+    # Create an edge trace
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines'
+    )
+
+    # Add traces to the figure
+    fig.add_trace(edge_trace)
+    fig.add_trace(node_trace)
+
+    # Update layout
+    fig.update_layout(
+        showlegend=False,
+        hovermode='closest',
+        title_text='Interactive {} PageRank Graph'.format(title),
+    )
+
+    return fig
+
+def flip(p):
+    return np.random.random() < p
+
+def random_walk(G, alpha=0.85, iters=1000):
+    counter = Counter()
+    node = next(iter(G))
+    
+    for _ in range(iters):
+        if flip(alpha):
+            node = np.random.choice(list(G[node]))
+        else:
+            node = np.random.choice(list(G))
+        
+        counter[node] += 1
+        
+    total = sum(counter.values())
+    for key in counter:
+        counter[key] /= total
+    return counter
+
 def recommend_similar_questions(title, num_recommendations=5):
     df = pd.read_csv("Data/leetcode_questions.csv")
 
     similar_questions = df[["Similar Questions ID", "Question Title", "Similar Questions Text"]]
 
-    # Handle missing text data by replacing NaN with an empty string
     similar_questions["Similar Questions Text"].fillna("", inplace=True)
 
     tfidf_vectorizer = TfidfVectorizer()
